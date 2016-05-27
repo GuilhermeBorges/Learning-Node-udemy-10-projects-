f9ddec0c8f14dcdc520b64db905f2b193363a275
Guilherme Oliveira <Guilherme Oliveira>
2016-05-27 11:13:52 -0300

    fixed the reference for css, it should start with a /

--- mailSender/routes/users.js
+++ mailSender/routes/users.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+/* GET users listing. */
+router.get('/register', function(req, res, next) {
+  res.render('contact',{
+  	'title':'Ola que tal'
+  });
+});
 
 module.exports = router;
