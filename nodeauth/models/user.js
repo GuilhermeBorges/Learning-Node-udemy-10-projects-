35f1740288569e2746be9bdd99e9aca283a6aed0
Guilherme Oliveira <Guilherme Oliveira>
2016-05-28 10:58:25 -0300

    Bcrypt was not added since i had some problems installing it. Video 17: Password Encryption With Bcrypt

--- nodeauth/models/user.js
+++ nodeauth/models/user.js
@@ -1,5 +1,5 @@
 var mongoose = require ('mongoose');
-
+// var bcrypt = require ('bcrypt');
 //telling wich database to use
 mongoose.connect('mongodb://localhost/nodeauth');
 
@@ -8,7 +8,7 @@ var db = mongoose.connection;
 // User Schema
 var UserSchema = mongoose.Schema({
 	username: {type:String, index:true},
-	password: {type:String},
+	password: {type:String, required:true/*, bcrypt:true*/},
 	email: {type:String},
 	name: {type:String},
 	profileImage: {type:String} //it is not the actually file but the path
@@ -19,5 +19,11 @@ var User = module.exports = mongoose.model('User',UserSchema);
 
 
 module.exports.createUser = function(newUser,callback){
+	// bcrypt.hash(newUser.password, 10, function(err,hash){
+	// 	if(err) throw err;
+	// 	//set hash password
+	// 	newUser.password = hash;
+	// 	newUser.save(callback); //removes the outside
+	// });//bcrypthash
 	newUser.save(callback);
 };//createuser
